6ecebcb6d4698359cfe97c26aa9a6ef17d817c33
morfeli <fmoreira95@hotmail.com>
2022-10-07 10:13:46 -0400

    tweaks

--- components/helper/useMelloContext.tsx
+++
@@ -1,8 +0,0 @@
-import { MelloContext } from "../store/mello-context";
-import { useContext } from "react";
-
-export const useMelloContext = () => {
-  const melloCtx = useContext(MelloContext);
-
-  return melloCtx;
-};
