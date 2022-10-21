9bbf1789d44a9040847b362e662c30af2fb8e8f1
morfeli <fmoreira95@hotmail.com>
2022-10-21 19:42:33 -0400

    tweaks

--- components/Menu/MenuCard.tsx
+++ components/Menu/MenuCard.tsx
@@ -10,12 +10,15 @@ type MenuCardProps = {
 export const MenuCard = ({ data, title }: MenuCardProps) => {
   return (
     <div className="flex flex-col pt-4">
-      <h1 className="self-center text-3xl py-2">{title}</h1>
-      {data.map((item) => (
-        <div className="p-4 border-b-2 border-b-hunter-green last:border-b-0">
+      <h1 className="self-center py-2 text-3xl">{title}</h1>
+      {data.map((item, i) => (
+        <div
+          key={i}
+          className="p-4 border-b-2 border-b-hunter-green last:border-b-0"
+        >
           <h1 className="text-2xl">{item.name}</h1>
           {item.description && (
-            <p className="text-lg py-2">{item.description}</p>
+            <p className="py-2 text-lg">{item.description}</p>
           )}
           <p className="text-lg">$ {item.price}</p>
         </div>
